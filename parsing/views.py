821b094489056050e512b46b574fbc924dc5169a
Vladimir Irkhin <ivb1989@yandex.ru>
2022-05-03 15:57:01 +0300

    Скорректировал обработку ошибок.

--- parsing/views.py
+++ parsing/views.py
@@ -1,6 +1,4 @@
 import json
-from json import JSONDecodeError
-
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Question
@@ -24,35 +22,34 @@ def questions_list(request):
     if request.method == 'POST':
         try:
             count = request.data['questions_num']
+            api_url = f'{URL}{count}'
+            response_data = requests.get(api_url).json()
+            data = [
+                {
+                    "id_question": res['id'],
+                    "question": res['question'],
+                    "answer": res['answer'],
+                    "created_at": res['created_at'],
+
+                }
+                for res in response_data
+            ]
+
+            serializer = QuestionSerializer(data=data, many=True)
+
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                print(data)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except json.decoder.JSONDecodeError as e:
             print("There was a problem accessing the equipment data.", e)
         except ValueError as e:
             print('Dont correct parameter questions_num', e)
-        except JSONDecodeError as e:
-            print('JSONDecodeError', e)
         except TypeError as e:
             print('TypeError', e)
-
-        api_url = f'{URL}{count}'
-        response_data = requests.get(api_url).json()
-        data = [
-            {
-                "id_question": res['id'],
-                "question": res['question'],
-                "answer": res['answer'],
-                "created_at": res['created_at'],
-
-            }
-            for res in response_data
-        ]
-
-        serializer = QuestionSerializer(data=data, many=True)
-
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            print(data)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            print('Other Exception', e)
 
 
         # return Response(data=serializer.data)
