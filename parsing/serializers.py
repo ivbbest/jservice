4d9043d8d7242ca58cb7a89d38a4b43cbf179f26
Vladimir Irkhin <ivb1989@yandex.ru>
2022-05-04 18:14:53 +0300

    Убрал лишние из файлов, что не используется. Добавил комментарии к основному функционалу.

--- parsing/serializers.py
+++ parsing/serializers.py
@@ -3,6 +3,9 @@ from .models import Question
 
 
 class QuestionSerializer(serializers.ModelSerializer):
+    """
+    Стандартный сериализатор для сериализации данных
+    """
     class Meta:
         model = Question
         fields = '__all__'
--- parsing/views.py
+++ parsing/views.py
@@ -1,8 +1,3 @@
-import json
-
-from django.http import Http404
-from django.shortcuts import render, get_object_or_404
-from rest_framework import generics
 from .models import Question
 from .serializers import QuestionSerializer
 from rest_framework.views import APIView
@@ -16,6 +11,10 @@ URL = 'https://jservice.io/api/random?count='
 
 
 def parser_questions(count):
+    """
+    Парсинг данных из 'https://jservice.io/api/random?count=' по заданному count
+    """
+
     api_url = f'{URL}{count}'
     response_data = requests.get(api_url).json()
     data = [
@@ -37,25 +36,39 @@ def parser_questions(count):
     return data, id_question
 
 
-class QuestionsView(APIView):
+class QuestionView(APIView):
+    """
+    Вьюшка для Question
+    """
     def post(self, request):
         count = request.data['questions_num']
 
+        # проверка, что задано целое число count, иначе вывод ошибки
         if not isinstance(count, int):
             raise ValueError("Don't correct questions_num. Repeat please!")
 
+        # вызов функции парсинга
         data, id_question = parser_questions(count)
+
+        # поиск одинаковых вопросов
         same_questions = Question.objects.filter(id_question__in=id_question).count()
 
+        # если есть одинаковые вопросы, то запускаем цикл, чтобы парсить новые вопросы
+        # пока не будет достигнута уникальность
         while same_questions > 0:
             data_new, id_question_new = parser_questions(same_questions)
             data += data_new
             same_questions = Question.objects.filter(id_question__in=id_question_new).count()
 
+        # передаем все на вход сериализатору
         serializer = QuestionSerializer(data=data, many=True)
 
+        # проверка валидации и сохранение в базу
         if serializer.is_valid(raise_exception=True):
             serializer.save()
+
+            # Ответ на запрос, который должен быть предыдущей сохранённый вопрос для викторины.
+            # В случае его отсутствия - пустой объект.
             previous_item_id = Question.objects.last().id-1
 
             if not Question.objects.filter(id=previous_item_id).exists():
